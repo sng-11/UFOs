e1b1ffaf8a79cf40bc9b7edc2f273fb47db69578
sng-11 <84816495+sng-11@users.noreply.github.com>
2021-09-12 22:11:04 -0400

    Add files via upload

---
+++ static/js/app_module.js
@@ -0,0 +1,48 @@
+let tbody = d3.select("tbody");
+
+// import the data from data.js
+const tableData = data;
+
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body; tells JS to find tbody tag in HTML and add a table row
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
+
+// `on` Function to attach an Event to the Handler Function
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Build Table with data.js 
+buildTable(tableData);
